ac095c278b153165b2f6b5148a0ff4cf1a9bb663
Rafael Barrientos <rafiprogramando@gmail.com>
2023-02-02 07:43:48 -0600

    Change to Mantequilla

---
+++ Mantequilla/src/data/Data.js
@@ -0,0 +1,6 @@
+const data = {
+  /* urlAPI: "http://localhost:8800", */
+  urlAPI: "https://www.winebackend.tk",
+};
+
+export default data;
---
+++ Mantequilla/src/data/Sections.js
@@ -0,0 +1,59 @@
+let sections = [
+  {
+    id: 1,
+    topic: "Explorando",
+    explanation:
+      "Descubre lo mejor de cada lugar para visitar y adentrate en historias únicas e inigualables. Cultura y tradición para explorar y presumir con tus amigos.",
+    section: "ver&escuchar",
+    articles: "http://localhost:8000/ver&escuchar",
+  },
+  {
+    id: 2,
+    topic: "Modo avión",
+    explanation:
+      "Disfruta de lo mejor en entretenimiento con música, videos, películas y toda la acción de los videojuegos para ponerte en modo avión junto a tus personas favoritas.",
+    section: "drinks&food",
+    articles: "http://localhost:8000/drinks&food",
+  },
+  {
+    id: 3,
+    topic: "Smart things",
+    explanation:
+      "¡Un mundo lleno de posibilidades!. Conoce y aprende de lo último en desarrollo tecnológico.",
+    section: "apps&gadgets",
+    articles: "http://localhost:8000/apps&gadgets",
+  },
+  {
+    id: 4,
+    topic: "Newbies",
+    explanation:
+      "Conoce los siguientes líderes y descubre coda uno de sus logros como profesionales.",
+    section: "socialite",
+    articles: "http://localhost:8000/socialite",
+  },
+  {
+    id: 5,
+    topic: "Tips",
+    explanation:
+      "Lo mejor de lo mejor para el cuidado de tu salud. Descubre consejos de todo tipo para el bienestar de tus seres queridos y propio.",
+    section: "bienestar",
+    articles: "http://localhost:8000/bienestar",
+  },
+  {
+    id: 6,
+    topic: "Estilo",
+    explanation: "Muéstrate al mundo con lo más nuevo en moda.",
+    section: "business",
+    articles: "http://localhost:8000/business",
+  },
+  {
+    id: 7,
+    topic: "Melancolía",
+    explanation:
+      "Activa el modo melancólico recordando eventos del pasado que dejaron marcada tu vida.",
+    section: "accion",
+    articles: "http://localhost:8000/accion",
+  },
+];
+
+export default sections;
